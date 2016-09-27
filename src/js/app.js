9ae4b0dd3406648647c270b7b4a3e360fd613382
Sam Goldsmith <samggold@gmail.com>
2016-09-27 15:57:24 -0600

    update to views and events

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ var app = angular.module('Beersportme', [
   'Beersportme.controllers.Profile_Form',
   'Beersportme.controllers.Profile_Edit_Form',
   'Beersportme.controllers.getAllTeams',
-  'Beersportme.controllers.getAllSports',
+  'Beersportme.controllers.getAllSports'
 ]);
 
 app.config(function($routeProvider) {
