d511aa466b674369a7dcfa517b4a41aba32c2e37
Alex <alnye655321@gmail.com>
2016-09-29 19:20:45 -0600

    fixed my events accordian

--- src/js/controllers/get_my_events_controller.js
+++ src/js/controllers/get_my_events_controller.js
@@ -4,8 +4,12 @@ angular.module('Beersportme.controllers.getMyEvents', [])
   var userID = $rootScope.userID;
   var myDataPromise = getFactory.getData('events/myevents/1');
   myDataPromise.then(function(result) {
-   // this is only run after getData() resolves
-   $scope.Get_Data = result;
+   // this is only run after getData() resolves Get_Data.data.events  --> result.data.events
+   var resultResponse = result;
+   for (var i = 0; i < resultResponse.data.events.length; i++) {
+     resultResponse.data.events[i].id = i;
+   }
+   $scope.Get_Data = resultResponse;
    //console.log("data.name"+$scope.data.name);
   });
 });
