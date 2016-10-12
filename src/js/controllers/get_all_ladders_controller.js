adbd44e89e620a36467e639163cac8b8b9433f8c
Tyler Maier <tyler_maier@yahoo.com>
2016-10-12 13:45:48 -0600

    ladder view

--- src/js/controllers/get_all_ladders_controller.js
+++ src/js/controllers/get_all_ladders_controller.js
@@ -1,35 +1,29 @@
 angular.module('Beersportme.controllers.getAllLadders', [])
-.controller("getAllLadders", function ($scope, getFactory, postFactory)
+.controller("getAllLadders", function($scope, getFactory, postFactory)
 {
-  // var myDataPromise = getFactory.getData('ladders');
-  // myDataPromise.then(function(result) {
-  //
-  //  // this is only run after getData() resolves
-  //  $scope.Get_Data = result;
-  //  //console.log("data.name"+$scope.data.name);
-  // });
 
-  var myDataPromise2 = getFactory.getData('ladders'); //!!! this is janky, get rid of big call and do individual ones on modal loads --> /super_table/:id !!!
-  myDataPromise2.then(function(result) {
-
-   // this is only run after getData() resolves
-   $scope.Get_SuperData = result;
-   //console.log("data.name"+$scope.data.name);
+  var myDataPromise2 = getFactory.getData('ladders')
+  .then(function(result) {
+    $scope.Get_Data = result;
   });
 
-  $scope.registerClickModal = function(ladderName) {
-    $scope.modalLadderName = ladderName;
+  $scope.registerClickModal = function(tableName) {
+    $scope.modalLadderName = tableName;
+
+    var ladderData = getFactory.getData('ladders/ladder/' + tableName)
+    .then(function(result) {
+    $scope.Ladder_Data = result;
+    });
   };
 
   $scope.registerSingleLadder = function(ladderID, tableName, userID) {
-    // console.log(userID);
     var payload = {
       player_id: userID,
       ladder_id: ladderID,
       table_name: tableName
     };
-    var myDataPromise = postFactory.postData('ladders/register', payload);
-    myDataPromise.then(function(result) {
+    var myDataPromise = postFactory.postData('ladders/register', payload)
+    .then(function(result) {
 
      // this is only run after postData() resolves, result is the status
      //$scope.SuccessPopup = somethin;
